56897db00a551de87d2db1895c1560e21aa48426
Anthony <anthonylaurent.lr@gmail.com>
2022-03-03 14:05:26 +0100

    Create ExchangeTestCase.swift

---
+++ Le Baluchon2/Le BaluchonTests/Exchange/ExchangeTestCase.swift
@@ -0,0 +1,29 @@
+//
+//  ExchangeResponseCase.swift
+//  Le BaluchonTests
+//
+//  Created by Anthony Laurent on 28/02/2022.
+//
+
+import XCTest
+@testable import Le_Baluchon
+
+class ExchangeTestCase: XCTestCase {
+//    func testUpdateRatesFailedCallbackIfError() {
+//        // Given
+//        let exchangeService = ExchangeService( exchangeSession: URLSessionFake(data: nil, response: nil, error: FakeExchangeResponseData.error))
+//
+//        // When
+//        let expectation = XCTestExpectation(description: "Wait for queue change.")
+//        exchangeService.updateRates { (success, ) in
+//            // Then
+//            XCTAssertFalse(success)
+//            XCTAssertNil(    )
+//            expectation.fulfill()
+//        }
+//
+//        wait(for: [expectation], timeout: 0.01)
+//        
+//    }
+    
+}
