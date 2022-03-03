83862fcf9c0224eac4d52a0cabf4a08c3b87396e
Anthony <anthonylaurent.lr@gmail.com>
2022-03-03 14:05:27 +0100

    Create FakeExchangeResponseData.swift

---
+++ Le Baluchon2/Le BaluchonTests/Exchange/FakeExchangeResponseData.swift
@@ -0,0 +1,31 @@
+//
+//  FakeResponseData.swift
+//  Le BaluchonTests
+//
+//  Created by Anthony Laurent on 28/02/2022.
+//
+
+import Foundation
+
+class FakeExchangeResponseData {
+    
+    static var exchangeCorrectData : Data? {
+        let bundle = Bundle(for: FakeExchangeResponseData.self)
+        let url = bundle.url(forResource: "Exchange", withExtension: "json")
+        return try! Data(contentsOf: url!)
+    }
+    
+    static let exchangeIncorrectData = "erreur".data(using: .utf8)!
+    
+    
+    static let responseOK = HTTPURLResponse(
+        url: URL(string: "https://openclassrooms.com")!,
+        statusCode: 200, httpVersion: nil, headerFields: nil)!
+
+    static let responseKO = HTTPURLResponse(
+        url: URL(string: "https://openclassrooms.com")!,
+        statusCode: 500, httpVersion: nil, headerFields: nil)!
+    
+    class ExchangeError: Error {}
+    static let error = ExchangeError()
+}
