a55c21904119d7f5be09356b20a7524a046f6746
Anthony <anthonylaurent.lr@gmail.com>
2022-03-03 14:03:08 +0100

    Create Currency.swift

---
+++ Le Baluchon2/Le Baluchon/Model/Exchange/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currencies.swift
+//  Le Baluchon
+//
+//  Created by Anthony Laurent on 15/02/2022.
+//
+
+import Foundation
+
+struct Currency {
+    
+    var name: String
+    var rate: Double
+}
