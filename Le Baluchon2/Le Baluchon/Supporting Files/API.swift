6ad85643a2042091056186de4c4592829d63e9a2
Anthony <anthonylaurent.lr@gmail.com>
2022-03-09 13:40:32 +0100

    Update API.swift

--- Le Baluchon2/Le Baluchon/Supporting Files/API.swift
+++ Le Baluchon2/Le Baluchon/Supporting Files/API.swift
@@ -10,17 +10,14 @@ import Foundation
 struct API {
     
     struct FixerAPI {
-        static let baseURL = "http://data.fixer.io/api/latest?"
-        static let APIKey = "526d3e48bd3fa11ca6029c1a5d1682dd"
-        
+        static let baseURL = "http://data.fixer.io/api/latest?access_key=526d3e48bd3fa11ca6029c1a5d1682dd&base=EUR)"
     }
     
-    struct GoogleTranslateAPI {
-        
+    struct TranslateAPI {
+        static let baseURL = "https://api.mymemory.translated.net/get?q=Bonjour&langpair=fr-CA|en-GB"
     }
     
     struct OpenWeatherMap {
-        static let baseURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}"
-        static let APIKey = "5473d0019b4ef195036ca00c8cc3e516"
+        static let baseURL = "https://api.openweathermap.org/data/2.5/group?id=5128638,2984512&APPID=5473d0019b4ef195036ca00c8cc3e516&lang=fr&units=metric"
     }
 }
