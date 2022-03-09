039f37a1a04f76082760a43e87ee578a5a9caac6
Anthony <anthonylaurent.lr@gmail.com>
2022-03-09 13:40:12 +0100

    Update ExchangeTestCase.swift

--- Le Baluchon2/Le BaluchonTests/Exchange/ExchangeTestCase.swift
+++ Le Baluchon2/Le BaluchonTests/Exchange/ExchangeTestCase.swift
@@ -9,21 +9,96 @@ import XCTest
 @testable import Le_Baluchon
 
 class ExchangeTestCase: XCTestCase {
-//    func testUpdateRatesFailedCallbackIfError() {
-//        // Given
-//        let exchangeService = ExchangeService( exchangeSession: URLSessionFake(data: nil, response: nil, error: FakeExchangeResponseData.error))
+    func testGetExchangeFailedCallbackIfError() {
+        // Given
+        let exchangeService = ExchangeService( exchangeSession: URLSessionFake(data: nil, response: nil, error: FakeExchangeResponseData.error))
+
+        // When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        exchangeService.getExchange { (success,Currency) in
+            // Then
+            
+            
+            XCTAssertFalse(success)
+            XCTAssertNotNil(Currency)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 0.01)
+
+    }
 //
-//        // When
-//        let expectation = XCTestExpectation(description: "Wait for queue change.")
-//        exchangeService.updateRates { (success, ) in
-//            // Then
-//            XCTAssertFalse(success)
-//            XCTAssertNil(    )
-//            expectation.fulfill()
-//        }
-//
-//        wait(for: [expectation], timeout: 0.01)
-//        
-//    }
-    
+    func testGetExchangeFailedCallbackIfNoData() {
+        // Given
+        let exchangeService = ExchangeService( exchangeSession: URLSessionFake(data: nil, response: nil, error: nil))
+
+        // When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        exchangeService.getExchange { (success, Currency) in
+            // Then
+            XCTAssertFalse(success)
+            XCTAssertNotNil(Currency)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 0.01)
+
+    }
+
+    func testGetExchangeFailedCallbackIfIncorrectResponse() {
+        // Given
+        let exchangeService = ExchangeService( exchangeSession: URLSessionFake(data: FakeExchangeResponseData.exchangeCorrectData, response: FakeExchangeResponseData.responseKO, error: nil))
+
+        // When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        exchangeService.getExchange { (success, Currency) in
+            // Then
+            XCTAssertFalse(success)
+            XCTAssertNotNil(Currency)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 0.01)
+
+    }
+
+    func testGetExchangeFailedCallbackIfIncorrectData() {
+        // Given
+        let exchangeService = ExchangeService( exchangeSession: URLSessionFake(data: FakeExchangeResponseData.exchangeIncorrectData, response: FakeExchangeResponseData.responseOK, error: nil))
+
+        // When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        exchangeService.getExchange { (success, Currency) in
+            // Then
+            XCTAssertFalse(success)
+            XCTAssertNotNil(Currency)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 0.01)
+
+    }
+
+    func testGetExchangeFailedCallbackIfNoErrorAndCorrectData() {
+        // Given
+        let exchangeService = ExchangeService( exchangeSession: URLSessionFake(data: FakeExchangeResponseData.exchangeCorrectData, response: FakeExchangeResponseData.responseOK, error: nil))
+
+        // When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        exchangeService.getExchange { (success, Currency) in
+            // Then
+            let name = "AED"
+            let rate = 4.115416
+
+            XCTAssertTrue(success)
+            XCTAssertNotNil(Currency)
+//            XCTAssertEqual(name, Currency[0].name)
+//            XCTAssertEqual(rate, Currency[0].rate)
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 0.01)
+
+    }
 }
